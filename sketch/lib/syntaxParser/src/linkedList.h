47c22c2fc525e826eb816bc13c64439861ecf0bb
Slash <dasilvaca@unal.edu.co>
2025-05-25 23:44:02 -0500

    Add LinkedList class, fully tested with basic operations (#3)

--- sketch/lib/syntaxParser/src/linkedList.h
+++ sketch/lib/syntaxParser/src/linkedList.h
@@ -1,7 +1,25 @@
 #pragma once
 
-template<typename NodeType>
+#include "node.hpp"
+
+template <typename ValueType>
 class LinkedList {
-    private:
-        struct Node
+ private:
+  Node<ValueType>* head;
+
+ public:
+  LinkedList();
+  ~LinkedList();
+
+  void insertFront(const ValueType& value);
+  Node<ValueType>* find(const ValueType& value) const;
+  bool getFound(const ValueType& value, ValueType& out) const;
+
+  void deleteValue(const ValueType& value);
+
+  void clear();
+  Node<ValueType>* getHead() const;
+  bool isEmpty() const;
 };
+
+#include "linkedList.tpp"
---
+++ sketch/lib/syntaxParser/tests/testLinkedList.cpp
@@ -0,0 +1,43 @@
+#define CATCH_CONFIG_MAIN
+#include "../src/linkedList.h"
+
+#include "../external/catch.hpp"
+
+class LinkedListValueFixture {
+public:
+    LinkedListValueFixture() {
+        for (char i = 'A'; i <= 'Z'; ++i) {
+            values.push_back(i);
+        }
+    }
+
+    std::vector<char> values;
+};
+
+TEST_CASE_METHOD(LinkedListValueFixture, "LinkedList insert, find, getFound and clear") {
+    LinkedList<char> list;
+    for (const char& value : values) {
+        list.insertFront(value);
+    }
+    for (const char& value : values) {
+        Node<char>* foundNode = list.find(value);
+        REQUIRE(foundNode != nullptr);
+        REQUIRE(foundNode->getValue() == value);
+    }
+    char foundValue;
+    for (const char& value : values) {
+        REQUIRE(list.getFound(value, foundValue));
+        REQUIRE(foundValue == value);
+    }
+    REQUIRE(list.getFound('!', foundValue) == false);
+    REQUIRE(foundValue != '!');
+    REQUIRE(list.getHead() != nullptr);
+
+    char deletedValue = values[0];
+    list.deleteValue(deletedValue);
+    REQUIRE(list.getFound(deletedValue, foundValue) == false);
+
+    list.clear();
+    REQUIRE(list.getHead() == nullptr);
+    REQUIRE(list.isEmpty() == true);
+}
